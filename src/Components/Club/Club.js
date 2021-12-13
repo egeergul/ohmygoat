6007a76218ea7e4fa099f2487c58c90498128357
egeergul <egeergull2001@gmail.com>
2021-12-13 23:21:48 +0300

    ege profile ve club düzeltildi

--- src/Components/Club/Club.js
+++ src/Components/Club/Club.js
@@ -1,29 +1,33 @@
 import React from 'react'
 import "./Club.css"
 
-const Club = () => {
+const Club = (props) => {
     return (
         <div>
             <div className="club-container">
                 <div className="club-header">
-                   <p>Chess Club</p>
+                    <img className="pp" src={props.pp}/>
+                    <p>{props.name}</p>
                 </div>
                 <div className="club-body">
-                    <img src="https://www.ecpps.k12.nc.us/uploaded/Elizabeth_City_Pasquotank_Early_College/clubs/chess-club.jpg"/>
-                    <div className="club-body-right">
+                    <p>Chess Club | {props.upcoming_events} Events | {props.total_events} Total Events</p>
+                    <div className="container">
+                    <div className="club-body-bottom row">
+                        <div className="col-md-8 club-body-left">
+                            <p>
+                            {props.description}  </p>
+                        </div>
 
-                        <p>Chess Club | 2 Upcoming Events | 15 Total Events
-This club consists of the chess lovers and, here we organize chess tournements and activities throughout the semester in the campus.
-
-                        </p>
-                        <button className="btn btn-primary btn-block">Exit</button>
-                        
-                        <button className="btn btn-primary btn-block">Visit Club</button>
+                        <div className="col-md-2 club-body-right">
+                            <button className="btn btn-primary btn-block">Exit</button>
+                            <button className="btn btn-primary btn-block">Visit Club</button>
+                        </div>
+                    </div>
                     </div>
 
                 </div>
             </div>
-            
+
         </div>
     )
 }
--- src/Pages/Student/Profile/Profile.js
+++ src/Pages/Student/Profile/Profile.js
@@ -1,34 +1,106 @@
 import React from 'react'
 import "./Profile.css"
-import {StNav, Calendar, Event, Club} from "../../../Components"
-import {useHistory} from  "react-router-dom";
+import {StNav, Event, Club} from "../../../Components"
+import {useHistory} from "react-router-dom";
 
+const events = [
+    {club:"ACM", name:"Tech Summit", mode: "Only Club Members",
+     description: "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.",
+     img: "https://render.fineartamerica.com/images/rendered/default/poster/10/8/break/images/artworkimages/medium/1/retro-travel-autumn-landscape-sassan-filsoof.jpg" 
+    },
+    {club:"YES", name:"YES BT", mode: "Public",
+     description: "Adipisicing eu laborum ad ullamco exercitation deserunt sit deserunt veniam adipisicing.",
+     img: "https://thumbs.dreamstime.com/z/green-garden-park-landscape-architecture-poster-landscaping-gardening-service-template-tree-plant-grass-120301540.jpg"  },
+    {club:"OR", name:"OR Fest", mode: "Only Director Board", 
+      description: "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.",
+    img:" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGiLoVwdejIfbG4sqBQ1XLOWTod5pb3PIUGA&usqp=CAU"  },
+    {club:"TDP", name:"Yürüyüş Yapalım", mode: "Public", 
+     description: "Yürüyüş yapalım spor spor!" ,
+     img: "https://productimages.hepsiburada.net/s/50/375/11037982130226.jpg" }
+   
+]
 
+const clubs = [
+    {name:"ACM", upcoming_events:3, total_events:13,
+     description: "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.",
+     pp: "https://render.fineartamerica.com/images/rendered/default/poster/10/8/break/images/artworkimages/medium/1/retro-travel-autumn-landscape-sassan-filsoof.jpg" 
+    },
+    {name:"YES", upcoming_events:3, total_events:13,
+     description: "Adipisicing eu laborum ad ullamco exercitation deserunt sit deserunt veniam adipisicing.",
+     pp: "https://thumbs.dreamstime.com/z/green-garden-park-landscape-architecture-poster-landscaping-gardening-service-template-tree-plant-grass-120301540.jpg"  },
+    {name:"OR",upcoming_events:3, total_events:13,
+      description: "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.",
+    pp:" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGiLoVwdejIfbG4sqBQ1XLOWTod5pb3PIUGA&usqp=CAU"  },
+    {name:"TDP", upcoming_events:3, total_events:13,
+     description: "Yürüyüş yapalım spor spor!" ,
+     pp: "https://productimages.hepsiburada.net/s/50/375/11037982130226.jpg" }
+   
+]
 
 const Profile = () => {
+    
 
-    let history =   useHistory()
     return (
         <div className="st-body-grid">
-            <div className="st-body-flex">
+            <div className="nav_cont">
                 <StNav/>
-                <div className="st-home-container">
-                    <div className="st-home-container-clubs2"> 
-                        <Club/>
-                        <Club/>
-                        <Club/>
-                        <Club/>
+            </div>
+            <div className="flex_cont">
+                <button className='del_my_acc'>
+                    Delete My Account
+                </button>
+
+                <div className="container">
+                    <div className="row header_bio">
+                        <div className="col-md-4 header_left">
+                            <img className="pp_class" src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/66/669b47d15b87c16c9cd429a4e29a064dac892e01_full.jpg"/>
+                        </div>
+                        <div className="col-md-8 ">
+                            <div className="header_right">
+                                <h3>can ozan</h3>
+                                <p>GE 250 taken</p>
+                                <p>
+                                    <span>bio:
+                                    </span>
+                                    Cupidatat ipsum cillum aute mollit duis.</p>
+                                <button>Edit Profile</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="st-home-container-events2"> 
-                        <Event/>
-                        <Event/>
-                    </div>
-                        
-                        
-                 
                 </div>
             </div>
+
+            <div className="container">
+                <div className="row">
+                <div className="profile_clubs col-lg-6">
+                {
+                                clubs.map((club)=>(<Club
+                                    
+                                    name={club.name}
+                                    upcoming_events={club.upcoming_events}
+                                    total_events={club.total_events}
+                                    description={club.description}
+                                    pp={club.pp}
+                                    />))
+                            }
+            </div>
+            <div className="profile_events col-lg-6">
+            {
+                                events.map((event)=>(<Event 
+                                    club={event.club}
+                                    name={event.name}
+                                    mode={event.mode}
+                                    description={event.description}
+                                    img={event.img}
+                                    />))
+                            }
+            </div>
+                </div>
+            </div>
+
         </div>
+
+
     )
 }
 
