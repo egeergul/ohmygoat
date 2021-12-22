c37ea976ab34040c6bfcbdbe4a15b3f54b1d041b
egeergul <egeergull2001@gmail.com>
2021-12-22 19:55:38 +0300

    club profile ve forum added

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,6 +3,8 @@ import ClNav from "./ClNav/ClNav"
 import AdmNav from "./AdmNav/AdmNav"
 import AdvNav from "./AdvNav/AdvNav"
 import Club from "./Club/Club"
+import Message from "./Message/Message"
+import Forum from "./Forum/Forum"
 import AdminClub from "./Club/AdminClub"
 import Event from "./Event/Event"
 import Assignment from "./Assignment/Assignment"
@@ -12,6 +14,6 @@ import StudentPreview from "./StudentPreview/StudentPreview"
 
 export {StNav, Event, Notification, Club, AdminClub,
     AdmNav, Assignment,  StudentPreview, ClubProfile, AdvNav,
-    ClNav}
+    ClNav, Forum, Message}
 
 
