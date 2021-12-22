102c599d534eda0ff4e3cedca89ed39ba448c9a3
egeergul <egeergull2001@gmail.com>
2021-12-22 11:39:44 +0300

    ege remastered advisor

--- src/Pages/Admin/CreateClub/CreateClub.js
+++ src/Pages/Admin/CreateClub/CreateClub.js
@@ -1,7 +1,5 @@
 import React, {useState} from 'react'
 import "./CreateClub.css"
-import {AdminNavMid, ScrollableList, AdminClub} from "../../../Components"
-
 
 const advisors = [
     {
@@ -68,76 +66,59 @@ const advisors = [
         name: "vavava.z",
         department: "CS"
     }
-];
-let listItems = []
+];let listItems = []
+
 for (let i = 0; i < 10000; i++) {
-    listItems.push({id: i, content: i})
-}
-const CreateClub = (props) => {
-    return (
-        <div>
-            <div className="stm-body-grid">
-                <AdminNavMid/>
-                <div className="bbbx">
-                    <div className="createclub-container">
-                        <div className="createclub-header">
-                            <p>
-                                <b>
-                                    Create Club
-                                </b>
-                            </p>
+listItems.push({id: i, content: i})}const CreateClub = (props) => {
+const [clubName, setClubName] = useState("");
+
+
+return (
+    <div>
+        <div className="create-club">
+            <form className="text-center">
+                <div className="d-flex flex-column">
+                    {/*Step 1*/}
+                    <div className="step d-flex flex-column">
+                        <div className="d-flex flex-row">
+                            <p className='circle'>1</p>
+                            <p>Specify Club Name</p>
                         </div>
-
-                        <div className="createclub-body">
-                            <div className="dflex m-3 d-flex justify-content center">
-                                <div className='circle'>1<></>
-                                </div>
-                                <p>Specify Club Name</p>
-
-                                <div className='midcircle'>2<></>
-                                </div>
-                                <p>Assign An Available Advisor</p>
-                                <div className='midcircle'>3<></>
-                                </div>
-                                <p>Click create club</p>
-                            </div>
-                            <div className="dflex m-3 d-flex justify-content center">
-                                <div className="mt-3 clubbox center">
-                                    <input type="searchclub" className="create-name" name="" placeholder="Write Club Name"/>
-                                </div>
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-6005 body-right-club">
-                                            {
-                                            advisors.map((advisor) => (
-                                                <div className="nameButton">
-                                                    <input type="checkbox" id="advname" name="advname" value="Paneer"/>{
-                                                    advisor.name
-                                                } </div>
-                                            ))
-                                        } </div>
-                                    </div>
-                                </div>
-                                <div className="centered">
-                                    <b>OR</b>
-                                </div>
-                                <div className="mt-3 mailbox center">
-                                    <input type="searchclub" className="create-name" name="" placeholder="advisor email"/>
-                                </div>
-                            </div>
-                            <button className="btn btn-primary btn-block create-button">
-                                Create Club
-                            </button>
-
-                        </div>
-
+                        <label>
+                            <input type="text" className="mt-2 form-control" placeholder="Club's Name"
+                                onChange={
+                                    e => setClubName(e.target.value)
+                                }/>
+                        </label>
+                        <p>
+                            <i>Default password will be oh_my_goat_new_club</i>
+                        </p>
                     </div>
 
+                    {/*Step 2*/}
+                    <div className="step d-flex flex-column">
+                        <div className="d-flex flex-row">
+                            <p className='circle'>2</p>
+                            <p><i>Assign an available advisor or create a new one</i></p>
+                        </div>
+                        here wşll be advisor lists
+                        <h3 className='mt-1'>OR</h3>
+                        <label>
+                            <input type="text" className="mt-2 form-control" placeholder="New Advisor's Email"
+                                onChange={
+                                    e => setClubName(e.target.value)
+                                }/>
+                        </label>
+                        <p>
+                            <i>Default password will be oh_my_goat_new_advisor</i>
+                        </p>
 
+                    </div>                   
                 </div>
-            </div>
-        </div>
-    )
-}
+                <button className="mt-3 btn btn-primary btn-block" type="submit">Create Club</button>
+            </form>
 
+        </div>
+    </div>
+)}
 export default CreateClub
