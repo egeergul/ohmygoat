9f7d8cc161abc2abaa8af648daf1916eb8af3c2b
Doruk Onur Caliskan <63496036+dorukcaliskan02@users.noreply.github.com>
2021-12-25 00:20:30 +0300

    Do Assignments page

---
+++ src/Components/DoAssignment/DoAssignment.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./DoAssignments.css"
+import {Forum} from "../"
+
+const DoAssignment = () => {
+
+
+    return (
+        <div>
+            <div className="container">
+            <div className="row mt-5">
+                    <div className="pin">
+                        This Assginment is Assigned to Ege Ergul for a certain date.
+                    </div>
+                    <Forum/>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default DoAssignment
