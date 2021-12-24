2c41d72843fa31eba71465654dbb87ef1d3fe53a
emreerdal9 <emre.erdal@ug.bilkent.edu.tr>
2021-12-24 12:20:59 +0300

    join event bug fixed

--- src/Pages/Advisor/AdvHome/AdvHome.js
+++ src/Pages/Advisor/AdvHome/AdvHome.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Event} from "../../../Components"
 import "./AdvHome.css"
 
 const AdvHome = () => {
 
-    const feed = [
-        {club:"ACM", name:"Tech Summit", mode: "Only Club Members",
-         description: "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.",
-         img: "https://render.fineartamerica.com/images/rendered/default/poster/10/8/break/images/artworkimages/medium/1/retro-travel-autumn-landscape-sassan-filsoof.jpg" 
-        },
-        {club:"YES", name:"YES BT", mode: "Public",
-         description: "Adipisicing eu laborum ad ullamco exercitation deserunt sit deserunt veniam adipisicing.",
-         img: "https://thumbs.dreamstime.com/z/green-garden-park-landscape-architecture-poster-landscaping-gardening-service-template-tree-plant-grass-120301540.jpg"  },
-        {club:"OR", name:"OR Fest", mode: "Only Director Board", 
-          description: "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.",
-        img:" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGiLoVwdejIfbG4sqBQ1XLOWTod5pb3PIUGA&usqp=CAU"  },
-        {club:"TDP", name:"Yürüyüş Yapalım", mode: "Public", 
-         description: "Yürüyüş yapalım spor spor!" ,
-         img: "https://productimages.hepsiburada.net/s/50/375/11037982130226.jpg" }
-       
-    ]
+    const [feed, setFeed] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:8080/event/allEvents", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${
+                    localStorage.token
+                }`
+            },
+            credentials: "include"
+        }).then((r) => {
+            if (r.ok) {
+                console.log(r);
+                return r;
+            } else if (r.status === 401 || r.status === 403 || r.status === 500) {
+                return Promise.reject(new Error("Bir hata oluştu " + r.status));
+            } else {
+                return Promise.reject(new Error("Bilinmeyen bir hata oluştu."));
+            }
+        }).then((r) => r.json()).then((r) => {
+            console.log(r);
+            setFeed(r)
+
+        }).catch((e) => {
+            console.log(e.message);
+        });
+    }, []);
 
     return (
         <div> 
@@ -35,21 +47,36 @@ const AdvHome = () => {
             <div className="adv-home">
             {
             feed.map((event) => (
-                <Event club={
-                        event.club
-                    }
-                    name={
-                        event.name
-                    }
-                    quota={
-                        event.quota
-                    }
-                    description={
-                        event.description
-                    }
-                    img={
-                        event.img
-                    }/>
+                <Event eventId = {
+                    event.eventId
+                }
+                       club={
+                           event.club
+                       }
+                       name={
+                           event.name
+                       }
+                       quota={
+                           event.quota
+                       }
+                       remainingQuota = {
+                           event.remainingQuota
+                       }
+                       date={
+                           event.eventDate
+                       }
+                       description={
+                           event.description
+                       }
+                       img={
+                           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWFk6kSNgK6Mhq9vrL-P6vQrHp60fJKYXuADtH8VAgPYEatKTBFt741R3SgLK_OwkkMRQ&usqp=CAU"
+                       }
+                       startClock = {
+                           event.startClock
+                       }
+                       endClock = {
+                           event.endClock
+                       }/>
             ))
             } 
             </div>
